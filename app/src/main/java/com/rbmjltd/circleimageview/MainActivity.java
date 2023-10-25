636eb35f1be1e4a4bde166bb8fe27781906e0790
rbmjl <rbmjltd@gmail.com>
2023-10-25 10:32:52 +0600

    Initial commit

---
+++ app/src/main/java/com/rbmjltd/circleimageview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.rbmjltd.circleimageview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    CircleImageView circleImageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        circleImageView = findViewById(R.id.profile_image);
+        circleImageView.setImageResource(R.drawable.profile);
+
+    }
+}
